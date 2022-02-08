f26c10bc8211b7c3de02852ff2173c3d5236298e
raviMukti <shinjiravi@gmail.com>
2022-02-08 19:10:07 +0700

    Logger

---
+++ logger_test.go
@@ -0,0 +1,15 @@
+package traininggolanglogging
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/sirupsen/logrus"
+)
+
+func TestLogger(t *testing.T) {
+	logger := logrus.New()
+
+	logger.Println("Hello Logger")
+	fmt.Println("Hello Fmt")
+}
